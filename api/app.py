d826700341bc1e31c7c319eb47a4f5c3eb2bbd9d
Manraj Singh <manrajsinghgrover@gmail.com>
2016-11-19 16:22:29 +0530

    Add: application.yml file added

--- api/app.py
+++ api/app.py
@@ -1,11 +1,31 @@
+import requests
+import yaml
 from flask import Flask, render_template
 
 app = Flask(__name__, template_folder="../webapp", static_folder="../webapp/dist")
+
+with open("application.yaml", 'r') as config:
+    try:
+        print yaml.load(config)
+    except yaml.YAMLError as exc:
+        print exc 
 
 @app.route('/')
 @app.route('/index')
 def index():
     return render_template('index.html')
+
+@app.route('/login')
+def login():
+    pass
+
+@app.route('/callback')
+def callback():
+    pass
+
+@app.route('/logout')
+def logout():
+    pass
 
 if __name__ == '__main__':
     app.run(debug=True)
