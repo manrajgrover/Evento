95ae4757ef935b62aa6ea414e3e5837daa782a18
Manraj Singh <manrajsinghgrover@gmail.com>
2016-11-27 13:04:27 +0530

    Update: More custom messages for events

--- api/app.py
+++ api/app.py
@@ -5,7 +5,8 @@ import urlparse
 from pprint import pprint
 from flask import Flask, Response, render_template, request, redirect, url_for, session
 
-app = Flask(__name__, template_folder = "../webapp", static_folder = "../webapp/dist")
+app = Flask(__name__, template_folder="../webapp",
+            static_folder="../webapp/dist")
 
 CONFIG = None
 
@@ -15,15 +16,18 @@ with open("application.yml", 'r') as config:
     except yaml.YAMLError as error:
         print error
 
+
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('index.html', client_id = CONFIG['client_id'])
+    return render_template('index.html', client_id=CONFIG['client_id'])
 
-@app.route('/login')
+
+@app.route('/login', methods=['GET'])
 def login():
 
-    if (session.get('username') is not None) and (session.get('name') is not None):
+    if ((session.get('username') is not None) and
+            (session.get('name') is not None)):
         resp = {
             "error": False,
             "username": session['username'],
@@ -31,7 +35,7 @@ def login():
         }
 
         return Response(response=json.dumps(resp),
-                        status=200, \
+                        status=200,
                         mimetype="application/json")
     else:
         resp = {
@@ -39,20 +43,21 @@ def login():
             "message": "Please authorize"
         }
 
-        return Response(response = json.dumps(resp),
-                        status = 401, \
-                        mimetype = "application/json")
+        return Response(response=json.dumps(resp),
+                        status=401,
+                        mimetype="application/json")
 
-@app.route('/callback')
+
+@app.route('/callback', methods=['GET'])
 def callback():
     code = request.args.get("code")
     response = requests.post("https://github.com/login/oauth/access_token",
-                            data = {
-                                'client_id': CONFIG['client_id'],
-                                'client_secret': CONFIG['client_secret'],
-                                'code': code
-                            } \
-                        )
+                             data={
+                                 'client_id': CONFIG['client_id'],
+                                 'client_secret': CONFIG['client_secret'],
+                                 'code': code
+                             }
+                             )
 
     if response.status_code == requests.codes.ok:
 
@@ -62,19 +67,19 @@ def callback():
 
         if 'repo,user' in scope:
 
-            auth_val = "token {token}".format(token = access_token)
+            auth_val = "token {token}".format(token=access_token)
 
             header = {
                 "Authorization": auth_val
             }
 
-            r = requests.get('https://api.github.com/user', headers = header)
+            r = requests.get('https://api.github.com/user', headers=header)
             result = r.json()
 
             session['username'] = result['login']
             session['token'] = access_token
             session['name'] = result['name']
-            
+
             return redirect(url_for('index'))
         else:
             resp = {
@@ -83,51 +88,56 @@ def callback():
             }
 
             return Response(response=resp,
-                            status=403, \
+                            status=403,
                             mimetype="application/json")
     else:
         resp = {
             "error": True,
             "message": "Unauthorized"
         }
-        
-        return Response(response = resp,
-                        status = 401, \
-                        mimetype = "application/json")
+
+        return Response(response=resp,
+                        status=401,
+                        mimetype="application/json")
+
 
 def getEventDetails(data):
     response = {}
 
-    response['user_url'] = "https://github.com/{user}".format(user = data['actor']['login'])
+    response[
+        'user_url'] = "https://github.com/{user}".format(user=data['actor']['login'])
     response['image'] = data['actor']['avatar_url']
 
     repository = data['repo']['name']
 
     response['repository'] = repository
-    response['repository_url'] = "https://github.com/{repository}".format(repository = repository)
+    response[
+        'repository_url'] = "https://github.com/{repository}".format(repository=repository)
     response['timestamp'] = data['created_at']
 
     payload = data['payload']
 
     if data['type'] == "CommitCommentEvent":
 
-        message = "Made a commit comment '{comment}'".format(comment = payload['comment']['body'])
+        message = "Made a commit comment '{comment}'".format(
+            comment=payload['comment']['body'])
         response['message'] = message
         response['message_url'] = payload['comment']['html_url']
 
     elif data['type'] == "CreateEvent":
 
-        message = "Created a {ref_type}".format(ref_type = payload['ref_type'])
+        message = "Created a {ref_type}".format(ref_type=payload['ref_type'])
         response['message'] = message
 
     elif data['type'] == "DeleteEvent":
 
-        message = "Deleted a {ref_type}".format(ref_type = payload['ref_type'])
+        message = "Deleted a {ref_type}".format(ref_type=payload['ref_type'])
         response['message'] = message
 
     elif data['type'] == "DeploymentEvent":
 
-        message = "Deployed {ref} code to {environment}".format(ref = payload['deployment']['ref'], environment = payload['deployment']['environment'])
+        message = "Deployed {ref} code to {environment}".format(
+            ref=payload['deployment']['ref'], environment=payload['deployment']['environment'])
         response['message'] = message
 
     elif data['type'] == "DeploymentStatusEvent":
@@ -165,47 +175,68 @@ def getEventDetails(data):
     elif data['type'] == "PublicEvent":
         response['message'] = "Lorem"
     elif data['type'] == "PullRequestEvent":
-        response['message'] = "Lorem"
+        message = "{action} pull request #{number}".format(
+            action=payload['action'].title(), number=payload['number'])
+        response['message'] = message
+        response['message_url'] = payload['pull_request']['html_url']
     elif data['type'] == "PullRequestReviewEvent":
-        response['message'] = "Lorem"
+        message = "{action} a pull request review on #{number}".format(
+            action=payload['action'].title(), number=payload['pull_request']['number'])
+        response['message'] = message
+        response['message_url'] = payload['review']['html_url']
     elif data['type'] == "PullRequestReviewCommentEvent":
-        response['message'] = "Lorem"
+        message = "{action} a comment on pull request #{number}".format(
+            action=payload['action'].title(), number=payload['pull_request']['number'])
+        response['message'] = message
+        response['message_url'] = payload['comment']['html_url']
     elif data['type'] == "PushEvent":
-        response['message'] = "Lorem"
+        message = "Pushed {size} commit(s) to the repository".format(
+            size=payload['size'])
+        response['message'] = message
     elif data['type'] == "ReleaseEvent":
-        response['message'] = "Lorem"
+        message = "{action} the repository with tag {tag_name}".format(
+            action=payload['action'], tag_name=payload['release']['tag_name'])
+        response['message'] = message
     elif data['type'] == "RepositoryEvent":
-        response['message'] = "Lorem"
+        message = "{action} the repository".format(action=payload['action'])
+        response['message'] = message
     elif data['type'] == "StatusEvent":
-        response['message'] = "Lorem"
+        message = "Status of commit with sha {sha} changed to {status}".format(
+            sha=payload['sha'], status=payload['state'])
+        response['message'] = message
     elif data['type'] == "TeamAddEvent":
-        message = "{repository} was added to {team}".format(repository = payload['repository']['full_name'], team = payload['team']['name'])
+        message = "{repository} was added to {team}".format(
+            repository=payload['repository']['full_name'], team=payload['team']['name'])
         response['message'] = message
     elif data['type'] == "WatchEvent":
-        message = "{action} watching the repository".format(action = payload['action'].title())
+        message = "{action} watching the repository".format(
+            action=payload['action'].title())
         response['message'] = message
 
     return response
 
-@app.route('/events', methods = ['GET'])
+
+@app.route('/events', methods=['GET'])
 def events():
 
     if (session.get('username') is not None) and (session.get('name') is not None):
         username = session['username']
         access_token = session['token']
 
-        auth_val = "token {token}".format(token = access_token)
+        auth_val = "token {token}".format(token=access_token)
 
         header = {
             "Authorization": auth_val
         }
 
-        page = request.args.get('page') if request.args.get('page') is not None else 1
+        page = request.args.get('page') if request.args.get(
+            'page') is not None else 1
 
         print page
 
-        events_url = 'https://api.github.com/users/{username}/events?page={page}'.format(username = username, page = page)
-        result = requests.get(events_url, headers = header)
+        events_url = 'https://api.github.com/users/{username}/events?page={page}'.format(
+            username=username, page=page)
+        result = requests.get(events_url, headers=header)
 
         events = result.json()
 
@@ -214,11 +245,11 @@ def events():
             resp = []
 
             for event in events:
-                resp.append(getEventDetails(event)) 
-            
-            return Response(response = json.dumps(resp),
-                            status = 200, \
-                            mimetype = "application/json")
+                resp.append(getEventDetails(event))
+
+            return Response(response=json.dumps(resp),
+                            status=200,
+                            mimetype="application/json")
         else:
             resp = {
                 "error": True,
@@ -226,9 +257,9 @@ def events():
                 "message": "No more to show"
             }
 
-            return Response(response = json.dumps(resp),
-                            status = 404, \
-                            mimetype = "application/json")
+            return Response(response=json.dumps(resp),
+                            status=404,
+                            mimetype="application/json")
 
     else:
         resp = {
@@ -236,11 +267,12 @@ def events():
             "message": "Please authorize"
         }
 
-        return Response(response = json.dumps(resp),
-                        status = 401, \
-                        mimetype = "application/json")
+        return Response(response=json.dumps(resp),
+                        status=401,
+                        mimetype="application/json")
 
-@app.route('/logout', methods = ['POST'])
+
+@app.route('/logout', methods=['POST'])
 def logout():
     session.pop('username', None)
     session.pop('name', None)
@@ -248,5 +280,5 @@ def logout():
     return redirect(url_for('index'))
 
 if __name__ == '__main__':
-    app.secret_key = "HelloWorld"
-    app.run(debug = True)
+    app.secret_key = CONFIG['app_secret_key']
+    app.run(debug=True)
